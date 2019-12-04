f1fb832a334b44b2dc549626cc2d1d86a3991026
Marcus Downing <marcus@bang-on.net>
2019-12-04 22:17:44 +0000

    Poll results saved

--- poll/poll.js
+++ poll/poll.js
@@ -89,7 +89,85 @@ ${contact}
     var mailfile = "mail/"+date+'.msg';
     fs.writeFile(mailfile, message, 'utf8', () => {
     	console.log("File", mailfile, "written.");
-  		res.send('Result saved.');
+  		res.send(`<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="fonts/Exo2-family.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/overwatch.css">
+    <link rel="alternate" type="application/rss+xml" href="/feed.rss" />
+
+    <title>Basingstoke Esports</title>
+  </head>
+  <body>
+
+    <!-- main header -->
+    <header id='header'>
+        <div class='container'>
+            <nav class="navbar">
+                <div class='navbar-brand'>
+                    <span class='d-block d-sm-inline'><b>&nbsp; BASINGSTOKE ESPORTS</b></span>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+    <section id='about'>
+        <div class='container'>
+            <div class='box'>
+                <h2>Results Saved</h2>
+                <p>Your survey results have been saved. Thank you.</p>
+
+                <a class="btn btn-primary btn-lg" href="/" role="button">Go back</a>
+            </div>
+        </div>
+    </section>
+
+    </main>
+
+    <!-- main footer -->
+    <footer id='footer'>
+        <figure class="faded overlay-picture">
+            <picture>
+                <img src='images/overwatch/banner/ana-3840x1200.jpg' class='img-fluid'>
+            </picture>
+            <div class="overlay d-flex">
+
+                <div class='container align-self-center'>
+                    <div class='box'>
+                        <h4 class='text-center d-none d-sm-block'>
+                            BASINGSTOKE ESPORTS
+                        </h4>
+                        <p class='text-center'>
+                            Organised by <a href='https://www.basingstokeanimesociety.com/' class="text-nowrap">Basingstoke Anime Society</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </figure>
+    </footer>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-117436530-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-117436530-1');
+    </script>
+
+  </body>
+</html>
+
+            `);
 
     });
 });
